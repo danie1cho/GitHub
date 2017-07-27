9ef38f43be755daaa3a06d4021e1a80139dbadef
danie1cho <daniel.cwcho@gmail.com>
2017-07-27 11:08:30 +0800

    first commit

---
+++ MvcApplication6/MvcApplication6/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using MvcApplication6.Models;
+using System.Web.Mvc;
+
+namespace MvcApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Format(FormatViewModel data)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View(data);
+        }
+
+        public ActionResult Index(string name)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormatViewModel data)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("登入成功");
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcApplication6/MvcApplication6/Models/FormatViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MvcApplication6.Models
+{
+    public class FormatViewModel
+    {
+        [EmailAddress]
+        [Remote("CheckUserName", "Validate", ErrorMessage = "帳號不可包含以下字串 skilltree demo twMVC")]
+        [Required(ErrorMessage = "請輸入帳號")]
+        public string 帳號 { get; set; }
+
+        [UIHint("password")]
+        [StringLength(20, MinimumLength=4)]
+        [Required(ErrorMessage = "請輸入密碼")]
+        public string 密碼 { get; set; }
+
+    }
+
+}
